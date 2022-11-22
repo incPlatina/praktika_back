3b335515faa7719829a95d5f5697591672738a5f
incPlatina <sgrafin2002@mail.ru>
2022-11-22 21:55:42 +0300

    full auth

---
+++ validation/auth.php
@@ -0,0 +1,22 @@
+<?php
+$login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
+$password = filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
+
+
+$password = md5($password."wrgrwrhrh23424t4");
+
+$mysql = new mysqli('localhost','root','root','register_db');
+
+$result=$mysql->query("SELECT * FROM `users` WHERE `login`='$login' AND `password`='$password'");
+$user = $result->fetch_assoc();
+
+if(count($user)==0){
+    echo "Неправильный логин или пароль";
+    exit();
+}
+
+setcookie('user', $user['name'], time()+3600, "/");
+
+$mysql->close();
+header('Location: /');
+?>
