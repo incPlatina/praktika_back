6652d06d804eb763372eede10382ea89889c5c5d
incPlatina <sgrafin2002@mail.ru>
2022-11-23 15:41:33 +0300

    fix length

--- validation/check.php
+++ validation/check.php
@@ -3,7 +3,7 @@ $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
 $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
 $password = filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
 
-if(mb_strlen($login) < 5 || mb_strlen($login)>90){
+if(mb_strlen($login) < 3 || mb_strlen($login)>90){
     $login='no';
     header("Location: /Авторизация.php?login=$login");
     exit();
@@ -11,7 +11,7 @@ if(mb_strlen($login) < 5 || mb_strlen($login)>90){
     $name='no';
     header("Location: /Авторизация.php?name=$name");
     exit();
-}else if (mb_strlen($password) < 5 || mb_strlen($password)>50){
+}else if (mb_strlen($password) < 3 || mb_strlen($password)>50){
     $password='no';
     header("Location: /Авторизация.php?password=$password");
     exit();
